4745793909c7f884efe4c69b9304e3f4e81b187c
hungpham1406 <hung.pham14062003@hcmut.edu.vn>
2024-04-12 20:26:51 +0700

    Init code for clock TIM3

---
+++ Source_Code/Core/Inc/DS3231.h
@@ -0,0 +1,37 @@
+/*
+ * DS3231.h
+ *
+ *  Created on: Apr 5, 2024
+ *      Author: Asus
+ */
+
+#ifndef INC_DS3231_H_
+#define INC_DS3231_H_
+
+//#include "main.h"
+//#include "global.h"
+//#include "stm32f4xx_hal.h"
+//#include "stm32f4xx.h"
+//
+//#define DS3231_ADDRESS 0xD0
+//
+//uint8_t dec_To_Bcd(int val);
+//int bcd_To_Dec(uint8_t val);
+//
+//void set_Time(uint8_t sec, uint8_t min, uint8_t hour, uint8_t dow, uint8_t dom,
+//			  uint8_t month, uint8_t year);
+//void get_Time(void);
+//
+//typedef struct {
+//	uint8_t second;
+//	uint8_t minute;
+//	uint8_t hour;
+//	uint8_t dayOfWeek;
+//	uint8_t dayOfMonth;
+//	uint8_t month;
+//	uint8_t year;
+//} TIME;
+//
+//extern TIME time;
+
+#endif /* INC_DS3231_H_ */
